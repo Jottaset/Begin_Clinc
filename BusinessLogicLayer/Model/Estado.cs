995c9cbc35498390c006406727103f8a076dd1cd
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-01-24 20:10:07 -0400

    novo git

---
+++ BusinessLogicLayer/Model/Estado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace BusinessLogicLayer.Model
+{
+    public class Estado
+    {
+
+        public int    Id { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public string DtCadastro { get; set; }
+
+
+        public Estado()
+        {
+        }
+    }
+}
---
+++ BusinessLogicLayer/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BusinessLogicLayer
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
